894b3640b53ec54d2b51636c67c51ff7b78a499c
wenderson <wendrson22@gmail.com>
2021-04-05 11:44:44 -0300

    cadastro com sucesso e tela de listagem

--- _inserir_produto.php
+++ _inserir_produto.php
@@ -13,5 +13,15 @@ VALUES ($nroproduto,'$nomeproduto','$categoria',$quantidade, '$fornecedor')";
 
 $inserir = mysqli_query($conexao, $sql);
 
-
 ?>
+
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  
+      <div class="container" style="width: 500px; margin-top: 20px; text-align: center">
+
+          <h4>Produto adicionado com sucesso!</h4>
+
+              <div style="padding-top: 20px">
+                <a href="index.html" role="button" class="btn btn-sm btn-primary">Cadastar novo item</a> 
+              </div>
+      </div>
