a3d1a1f7e4d948489a048acc10477a54c9d733fb
wenderson <wendrson22@gmail.com>
2021-04-07 11:33:30 -0300

    estilização login page

--- index.php
+++ index.php
@@ -5,27 +5,39 @@
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Login</title>
+
+  <!-- Bootstrap -->
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+  <link rel="stylesheet" href="style/login.css">
+   <!-- Fontawesome -->
+  <script src="https://kit.fontawesome.com/af3dc3f21d.js" crossorigin="anonymous"></script>
+
 </head>
 
 <body>
-
-        <div class="container">
-          <form>
-                <div class="form-group">
-                  <label>Usuario</label>
-                  <input type="email" class="form-control" name="usuario" id="" placeholder="Entre com seu usuario">
+              <div class="container">
+                <div class="row m-5 no-gutters shadow-lg">
+                  <div class="col-md-6 d-none d-md-block">
+                    <img src="https://images.unsplash.com/photo-1597514110707-b988d3a08652?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=80" class="img-fluid" style="min-height:100%;" />
+                  </div>
+                      <div class="col-md-6 bg-white p-5">
+                        <h3 class="pb-3">Login</h3>
+                          <div class="form-style">
+                            <form>
+                              <div class="form-group pb-3">    
+                                <input type="email" placeholder="Usuario" class="form-control" id="" aria-describedby="email">   
+                              </div>
+                              <div class="form-group pb-3">   
+                                <input type="password" placeholder="Senha" class="form-control" id="">
+                              </div>
+                              <div class="pb-2">
+                                <button type="submit" class="btn btn-primary w-100 font-weight-bold mt-2">Entrar</button>
+                              </div>
+                            </form>
+                          </div>
+                      </div>
+                    </div>
                 </div>
-                <div class="form-group">
-                  <label>Senha</label>
-                  <input type="password" class="form-control" name="password" id="" placeholder="Entre com sua senha">
-                </div>
-                <button type="submit" class="btn btn-primary">Entrar</button>
-              </form> 
-          </div>
-
-
-
 
       <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
       <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
