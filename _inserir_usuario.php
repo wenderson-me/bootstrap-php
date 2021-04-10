377fe7116f82c4950277de3da6f61df4f41d2e5b
wenderson <wendrson22@gmail.com>
2021-04-10 11:58:10 -0300

    altos ajustes de layout

--- _inserir_usuario.php
+++ _inserir_usuario.php
@@ -17,13 +17,18 @@
     $inserir = mysqli_query($conexao, $sql);
 ?>
 
+<script src="https://kit.fontawesome.com/af3dc3f21d.js" crossorigin="anonymous"></script>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  
-      <div class="container" style="width: 500px; margin-top: 20px; text-align: center">
-
+<style>
+body {
+  background: #E5E5E5;
+}
+</style>
+  <body>
+        <div class="container shadow-sm p-3 mb-5 bg-white rounded" style="width: 500px; margin-top: 20px; text-align: center">
           <h4>Usuario cadastrado com sucesso!</h4>
-
               <div style="padding-top: 20px">
-                <a href="cadastro_usuario.php" role="button" class="btn btn-sm btn-primary">Voltar para cadastro</a> 
+              <a href="cadastro_usuario.php"  role="button" class="btn btn-primary btn-lg mt-4"><i class="fas fa-arrow-circle-left"></i></a> 
               </div>
-      </div>
+        </div>
+      </body>
